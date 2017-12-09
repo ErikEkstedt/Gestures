a07c8f8b911381ffead351da8729fdd0238f8b3b
Erik <eeckee@gmail.com>
2017-12-10 00:18:07 +0100

    update

--- plain_roboschool.py
+++ plain_roboschool.py
@@ -1,20 +1,22 @@
-# from OpenGL import GLX, GLE, GL  # fix for opengl issues on desktop  / nvidia
 from OpenGL import GL
 import gym, roboschool
 import numpy as np
 import time
 
-env = gym.make('RoboschoolHumanoid-v1')
+'''
+For me every state value becomes `nan` after reset() is called after render()
+has been called. It does not matter if render is only done once, after reset
+everything is `nan`. Can't get it to work. I get around this on my own custom
+environments.
+'''
 
+env = gym.make('RoboschoolHumanoid-v1')
 while True:
     print('\n------------')
     env.reset()
+    env.render()  # Does not seem to matter where this is.
     while True:
         s, r, d, _ = env.step(env.action_space.sample())
         # obs = env.render("rgb_array")
-        obs = env.render("human")
         if d:
             break
-
-env.close()
-del env
