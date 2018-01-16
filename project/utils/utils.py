9b4d7fe711411c817c0d395c3958be94851b274b
Erik <eeckee@gmail.com>
2018-01-16 10:16:22 +0100

    up

--- project/utils/utils.py
+++ project/utils/utils.py
@@ -137,23 +137,10 @@ def clip_gradient(model, clip_norm):
         if p.requires_grad:
             p.grad.mul_(norm)
 
-def vis_detections(im, class_name, dets, thresh=0.8):
-    """Visual debugging of detections."""
-    for i in range(np.minimum(10, dets.shape[0])):
-        bbox = tuple(int(np.round(x)) for x in dets[i, :4])
-        score = dets[i, -1]
-        if score > thresh:
-            cv2.rectangle(im, bbox[0:2], bbox[2:4], (0, 204, 0), 2)
-            cv2.putText(im, '%s: %.3f' % (class_name, score), (bbox[0], bbox[1] + 15), cv2.FONT_HERSHEY_PLAIN,
-                        1.0, (0, 0, 255), thickness=1)
-    return im
-
-
 def adjust_learning_rate(optimizer, decay=0.1):
     """Sets the learning rate to the initial LR decayed by 0.5 every 20 epochs"""
     for param_group in optimizer.param_groups:
         param_group['lr'] = decay * param_group['lr']
-
 
 def save_checkpoint(state, filename):
 torch.save(state, filename)
